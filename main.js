386c24ebcbdbf0da5c1706eb9f762ef8b4b2a50f
midlajt <muhammedmidlaj236@gmail.com>
2022-11-12 12:49:43 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const container=document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const SQUARES = 500
+
+for(let i=0 ; i < SQUARES; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setcolor (square))
+
+    square.addEventListener('mouseout', () => removecolor(square))
+
+    container.appendChild(square)
+}
+
+function setcolor (element){
+    const color = getRandomcolor()
+    element.style.background = color
+    element.style.boxshow = `0 0 2px ${color}, 0 0 10px ${color}` 
+}
+
+function removecolor (element){
+    element.style.background = '#1d1d1d'
+    element.style.boxshow = `0 0 2px #000 `
+}
+
+function getRandomcolor (){
+    return colors[Math.floor(Math.random()* colors.length   )]
+}
